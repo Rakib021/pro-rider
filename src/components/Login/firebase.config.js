54d83db7c29bf2f49f0b85823f4b9db28ef61fc8
Rakib <rakib@Rakibs-MacBook-Pro.local>
2021-04-30 23:49:14 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6bWsnhavQsIhrI1bIeDBBg4Lmc53xoNw",
+  authDomain: "pro-riders-b489a.firebaseapp.com",
+  projectId: "pro-riders-b489a",
+  storageBucket: "pro-riders-b489a.appspot.com",
+  messagingSenderId: "791723509077",
+  appId: "1:791723509077:web:982b4c3e4c3b9fc26c5bdc"
+};
+  export default firebaseConfig;
